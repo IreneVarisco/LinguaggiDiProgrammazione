80b5299c8c10dcda8426d2d960be3cd1b33f5ea7
IreneVarisco <variscoirene@gmail.com>
2024-10-16 11:58:04 +0200

    lab02/es1 works

--- Laboratorio/OcaML/Lab02/Es1.ml
+++ Laboratorio/OcaML/Lab02/Es1.ml
@@ -6,19 +6,19 @@ let interdependency = ["friendship"; "kinship"; "financial exchange"; "dislike";
 
 module type makeGraph =
 sig
-  type ’a graph
-  val empty : unit -> ’a graph
-  val add_node : ’a -> ’a graph -> ’a graph
-  val add_arc : ’a -> ’a -> ’a graph -> ’a graph
-  val adjacents : ’a -> ’a graph -> ’a list
-    val node_is_in_graph : ’a -> ’a graph -> bool val is_empty : ’a graph -> bool
+  type 'a graph
+  val empty : unit -> 'a graph
+  val add_node : 'a -> 'a graph -> 'a graph
+  val add_arc : 'a -> 'a -> 'a graph -> 'a graph
+  val adjacents : 'a -> 'a graph -> 'a list
+    val node_is_in_graph : 'a -> 'a graph -> bool val is_empty : 'a graph -> bool
     exception TheGraphIsEmpty
     exception TheNodeIsNotInGraph
   end;;
 
   module SocialNetwork : makeGraph =
     struct 
-    type ’a graph = SocialNetwork of (’a list) * ((’a * ’a) list)
+    type 'a graph = SocialNetwork of ('a list) * (('a * 'a) list)
 
     let empty () = SocialNetwork ([], []) let is_empty = function
       SocialNetwork(nodes, _) -> (nodes = []) exception TheGraphIsEmpty
@@ -31,8 +31,8 @@ sig
     let node_is_in_graph n = function SocialNetwork(nodes, _) -> is_in_list n nodes
 
     let rec add_in_list  ?(res = []) x = function
-      [] :: List.rev x::res
-      | h :: tl when (h = x) -> List.rev_append tl(h :: res)
+      [] -> List.rev (x::res)
+      | h :: tl when (h = x) -> List.rev_append res (h :: tl)
       | h::tl -> add_in_list ~res:(h::res) x tl
 
       let add_node n = function
@@ -43,11 +43,9 @@ sig
         SocialNetwork(nodes, arcs) ->
           SocialNetwork( (add_in_list d (add_in_list s nodes)), (add_in_list (s,d) arcs) )
 
-      let adjacents n = 
-        let adjacents n 1 =
-          List.map snd (List.filter (fun x -> ((fst x) = n)) l) 
-        in function
-          SocialNetwork(_, arcs) -> adjacents n arcs
+      let adjacents n = function
+        SocialNetwork(_, arcs) ->
+          List.map snd (List.filter (fun (s, _) -> s = n) arcs)
 
     end;;
 
@@ -59,15 +57,29 @@ sig
       | (s,d)::tl -> arcs_to_graph (add_arc s d g) tl
       in arcs_to_graph (empty()) arcs
 
+      open SocialNetwork
+      let dfs s graph =
+        let rec dfs_aux list visited =
+          match list with
+          | [] -> visited
+          | node::tl ->
+            if List.mem node visited then
+              dfs_aux tl visited
+            else
+              let adj = adjacents node graph in
+              dfs_aux (adj @ tl) (node :: visited)
+        in
+        dfs_aux [s] []
 
     (* Example usage *)
 let () =
-  let open SocialNetwork in
-  let g = empty () in
-  let g = add_node {name="Alice"; surname="Smith"; age=30} g in
-  let g = add_node {name="Bob"; surname="Johnson"; age=25} g in
-  let g = add_arc {name="Alice"; surname="Smith"; age=30} {name="Bob"; surname="Johnson"; age=25} g in
-  let adj = adjacents {name="Alice"; surname="Smith"; age=30} g in
-  List.iter (fun ind -> Printf.printf "%s %s\n" ind.name ind.surname) adj
+let g = empty () in
+let g = add_node {name="Alice"; surname="Smith"; age=30} g in
+let g = add_node {name="Bob"; surname="Johnson"; age=25} g in
+let g = add_node {name="Charlie"; surname="Brown"; age=20} g in
+let g = add_arc {name="Alice"; surname="Smith"; age=30} {name="Bob"; surname="Johnson"; age=25} g in
+let g = add_arc {name="Bob"; surname="Johnson"; age=25} {name="Charlie"; surname="Brown"; age=20} g in
+let visited = dfs {name="Alice"; surname="Smith"; age=30} g in
+List.iter (fun ind -> Printf.printf "%s %s\n" ind.name ind.surname) visited
 
   
